b07534910aa86dc2e75630d5bf74e63b1d33303f
Serena-Dong <serenadong3159@icloud.com>
2022-12-05 16:41:42 +0100

    operations

--- js/script.js
+++ js/script.js
@@ -14,3 +14,37 @@ const age = prompt('Age?');
 
 // Chiedo all'utente la quantità dei chiolometri che vuole percorrere
 const distance = prompt('Quanti chilometri vuole percorrere?');
+
+const ticket = 0.21
+
+const a = age
+const x = distance 
+
+let amount = distance * ticket
+
+
+if (a >= 21){
+    message = 'maggiorenne';
+    if (a >= 65){
+        message = 'over';
+        message = ( amount - ( amount * 40 / 100));
+    } else {
+        message = 'under';
+        message = (amount);
+    }
+} else {
+    message = 'minorenne';
+    message = ( amount - ( amount * 20 / 100));
+
+}
+
+console.log(message)
+
+
+placeholder.innerText = (message);
+
+
+
+
+/* Colleziono i dati e li inserisco nella risposta
+placeholder.innerText = ; */
