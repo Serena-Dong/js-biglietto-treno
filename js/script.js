26fea0936e21b2361ad3b8ac6b6eca92e2348648
Serena-Dong <serenadong3159@icloud.com>
2022-12-05 15:48:53 +0100

    domande

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 /* 
 
-1
+1-questions
+2-operations
+3-result
 
 */
+
+// Preparo l'elemento che deve essere riempito
+const placeholder = document.getElementById('amount');
+
+// Chiedo all'utente l'età
+const age = prompt('Age?');
+
+// Chiedo all'utente la quantità dei chiolometri che vuole percorrere
+const distance = prompt('Quanti chilometri vuole percorrere?');
