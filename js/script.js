9b232be8329b648ae9889951e3e277d9a05a82b8
Serena-Dong <serenadong3159@icloud.com>
2022-12-05 17:44:12 +0100

    sconto in pagina

--- js/script.js
+++ js/script.js
@@ -9,8 +9,9 @@
 // Preparo l'elemento che deve essere riempito
 const placeholder = document.getElementById('amount');
 
+
 // Chiedo all'utente l'età
-const age = prompt('Age?');
+const age = prompt('Quanti anni hai?');
 
 // Chiedo all'utente la quantità dei chiolometri che vuole percorrere
 const distance = prompt('Quanti chilometri vuole percorrere?');
@@ -22,20 +23,26 @@ const a = age
 const x = distance 
 
 let price = distance * ticket
+const minorDiscount = price - 20 / 100
+const overDiscount = price - 40 / 100
 
 // Operazioni
 if (a >= 21){
     message = 'maggiorenne';
     if (a >= 65){
         message = 'over';
-        tot = ( price - ( price * 40 / 100));
+        tot = ( price - ( overDiscount));
+        document.getElementById("initial-amount").innerHTML ="Prezzo Iniziale: €", price;
+        document.getElementById("discount-amount").innerHTML = "Sconto Applicato: €", overDiscount.toFixed(2);
+
     } else {
         message = 'under';
         tot = (price);
     }
 } else {
     message = 'minorenne';
-    tot = ( price - ( price * 20 / 100));
+    tot = ( price - ( price * minorDiscount));
+    document.getElementById("initial-amount").innerHTML = price;
 
 }
 
@@ -43,6 +50,11 @@ console.log(message)
 
 // Colleziono i dati e li inserisco nella risposta
 placeholder.innerText = (tot.toFixed(2));
+
+
+
+
+
 
 
 
