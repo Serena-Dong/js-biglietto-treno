4473ae6729710e9024896c746c495bdb2330a309
Serena-Dong <serenadong3159@icloud.com>
2022-12-05 17:12:06 +0100

    massimo 2 decimali

--- js/script.js
+++ js/script.js
@@ -21,29 +21,28 @@ const ticket = 0.21
 const a = age
 const x = distance 
 
-let amount = distance * ticket
+let price = distance * ticket
 
 // Operazioni
 if (a >= 21){
     message = 'maggiorenne';
     if (a >= 65){
         message = 'over';
-        message = ( amount - ( amount * 40 / 100));
+        tot = ( price - ( price * 40 / 100));
     } else {
         message = 'under';
-        message = (amount);
+        tot = (price);
     }
 } else {
     message = 'minorenne';
-    message = ( amount - ( amount * 20 / 100));
+    tot = ( price - ( price * 20 / 100));
 
 }
 
 console.log(message)
 
-
 // Colleziono i dati e li inserisco nella risposta
-placeholder.innerText = (message);
+placeholder.innerText = (tot.toFixed(2));
 
 
 
